6464dc3a33ec054c8a9f2651fb48698261bd41eb
Lakshminarayanan R <lakshminarayanan@Lakshminarayanans-MacBook-Air.local>
2024-08-25 22:58:57 +0530

    added few UI elements

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ im = Image.open('dashboard.png')
 # Adding Image to web app
 st.set_page_config(page_title="Surge Price Prediction App", page_icon = im)
 
+st.title('Report Recommendation App')
+
+st.text('A compant has lots of dashboards and reports and it is important to get insights from the right report. This application helps the user identify the right report they are looking for.')
+
 @st.cache(allow_output_mutation=True)
 def get_model():
     tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
@@ -42,11 +46,13 @@ def cross_score(model_inputs):
     return scores
 
 
-user_input = st.text_area("Enter Text")
-button = st.button("Analyze")
+user_input = st.text_area("Provide a context of the Report that you wish to look")
+button = st.button("Fetch Report List")
 
 
 if user_input and button:
+    st.write('The model is fetching relevant reports for the given context -', user_input)
+    st.write("It might take a few minutes to load")
     model_inputs = [[user_input,item['KPI Descriptions']] for item in results]
     scores = cross_score(model_inputs)
     #Sort the scores in decreasing order
@@ -54,6 +60,11 @@ if user_input and button:
     ranked_results = sorted(ranked_results, key=lambda x: x['Score'], reverse=True)
 
     final_results = pd.DataFrame()
-    final_results['cross_encoder'] = [item['Dashboard Name'] for item in ranked_results[0:5]]
+    final_results['cross_encoder'] = [item['Dashboard Name'] for item in ranked_results[0:3]]
+
+    df_result = df[df['Dashboard Name'].isin([item['Dashboard Name'] for item in ranked_results[0:3]])]
+    df_result.rename(columns = {'KPI Description': 'About the Dashboard', 'KPI Values':'Key Insights'}, inplace = True)
+    df_result.drop(columns='KPIs', inplace = True)
+    df_result
 
     st.table(final_results)
